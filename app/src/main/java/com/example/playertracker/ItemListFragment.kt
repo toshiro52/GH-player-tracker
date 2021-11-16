0a3c04da8bdc093ec27e88bacc2b0e3498d23b7d
Paweł Głowacki-Grzyb <83398435+toshiro52@users.noreply.github.com>
2021-11-16 15:46:15 +0100

    AbilityCard, Perk, Item layouts implemented

--- app/src/main/java/com/example/playertracker/ItemListFragment.kt
+++ app/src/main/java/com/example/playertracker/ItemListFragment.kt
@@ -5,7 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.playertracker.databinding.FragmentItemListBinding
+import com.example.playertracker.databinding.FragmentPlayerStatBinding
 
 class ItemListFragment : Fragment() {
+    private var _binding: FragmentItemListBinding? = null
+    private val binding get() = _binding!!
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentItemListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/playertracker/PerkFragment.kt
+++ app/src/main/java/com/example/playertracker/PerkFragment.kt
@@ -5,7 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.playertracker.databinding.FragmentPerkBinding
 
 class PerkFragment : Fragment() {
+
+    private var _binding: FragmentPerkBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPerkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
